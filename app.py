64bc2b31fb80053e27e8873c976a7afa018f42ba
bazhil <i-bazh@mail.ru>
2022-07-23 15:19:18 +0300

    add dognail for start docker

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 import os
 import signal
 from flask import Flask
-from buzz import generator
+
+generator = None
+
+try:
+    from buzz import generator
+except:
+    import random
+
+    buzz = ('continuous testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
+    adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+    adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'seriously')
+    verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+
+
+    def sample(l, n=1):
+        result = random.sample(l, n)
+        if n == 1:
+            return result[0]
+        return result
+
+
+    def generate_buzz_worker():
+        buzz_terms = sample(buzz, 2)
+        phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs), sample(verbs), buzz_terms[1]])
+        return phrase.title()
 
 app = Flask(__name__)
 
@@ -11,7 +35,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 @app.route("/")
 def generate_buzz():
     page = '<html><body><h1>'
-    page += generator.generate_buzz()
+    page += generator.generate_buzz() if generator else generate_buzz_worker()
     page += '</h1></body></html>'
     return page
 
